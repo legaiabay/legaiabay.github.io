58eabb44aa2ca3f4e78726210852223224dc3633
legaiabay <abdul.k.fauzi@gmail.com>
2020-05-27 11:33:18 +0700

    testing

--- games/jumpingslime/mobile/main.1687c.js
+++ games/jumpingslime/mobile/main.1687c.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.35b1c.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.cc589.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.ca3b2.js');
+        require('src/settings.efda6.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.ca3b2.js');
+        require('src/settings.efda6.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
